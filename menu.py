89776bd489dcfad17628358f8cd64b9a2df23969
Mark Mellors <Mark.Mellors@gmail.com>
2017-11-04 09:11:47 +0000

    tidied up code, made touch areas match buttons, exit working

--- menu.py
+++ menu.py
@@ -13,31 +13,47 @@ def make_button(text, xpo, ypo, colour):
 	font=pygame.font.Font(None,24)
 	label=font.render(str(text), 1, (colour))
 	screen.blit(label,(xpo,ypo))
-	pygame.draw.rect(screen, cream, (xpo-5,ypo-5,200,30),1)
+	pygame.draw.rect(screen, cream, (xpo-5,ypo-5,100,65),1)
 
 #define function that checks for mouse location
 def on_click(mousepos):
-	click_pos = (mousepos) #(pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
+	click_pos = (mousepos)
 	#check to see if exit has been pressed
-	if 15 <= click_pos[0] <= 215 and 5 <= click_pos[1] <=35:
+	if 15 <= click_pos[0] <= 115 and 5 <= click_pos[1] <=70:
 		print "Straight Line challenge launched"
 		button(0)
 	#now check to see if button 1 was pressed
-	if 15 <= click_pos[0] <= 215 and 40 <= click_pos[1] <=70:
-                print "Minimal Maze launched"
+	if 15 <= click_pos[0] <= 115 and 75 <= click_pos[1] <=140:
+                print "Rainbow challenge launched"
                 button(1)	
 	#now check to see if button 2 was pressed
-        if 15 <= click_pos[0] <= 215 and 75 <= click_pos[1] <=105:
-                print "Rainbow launched"
+        if 15 <= click_pos[0] <= 115 and 145 <= click_pos[1] <=210:
+                print "Pi Noon challenge launched"
                 button(2)
 	#now check to see if button 3 was pressed
-        if 15 <= click_pos[0] <= 215 and 110 <= click_pos[1] <=140:
-                print "Golf launched"
+        if 15 <= click_pos[0] <= 115 and 215 <= click_pos[1] <=280:
+                print "Duck Shoot challnge launched"
                 button(3)
 	#now check to see if button 4 was pressed
-        if 15 <= click_pos[0] <= 215 and 145 <= click_pos[1] <=200:
-                print "Pi Noon Launched"
+        if 120 <= click_pos[0] <= 220 and 5 <= click_pos[1] <=70:
+                print "Minimal Maze challenge launched"
                 button(4)
+        #now check to see if button 5 was pressed
+        if 120 <= click_pos[0] <= 220 and 75 <= click_pos[1] <=140:
+                print "Golf challenge launched"
+                button(5)
+        #now check to see if button 6 was pressed
+        if 120 <= click_pos[0] <= 220 and 145 <= click_pos[1] <=210:
+                print "Obstacle Course challenge launched"
+                button(6)
+        #now check to see if button 7 was pressed
+        if 120 <= click_pos[0] <= 220 and 215 <= click_pos[1] <=280:
+                print "Radio control mode launched"
+                button(7)
+	#now check to see if button 8 was pressed
+        if 15 <= click_pos[0] <= 115 and 285 <= click_pos[1] <=320:
+                print "Exit selected"
+                button(8)
 
 #define action on pressing buttons
 def button(number):
@@ -51,21 +67,10 @@ def button(number):
 		time.sleep(1)
 		#sys.exit()
 
-	if number == 1:		
+	if number == 8:		
                 time.sleep(1) #do something interesting here
-     		#sys.exit()
-		
-	if number == 2:		
-		time.sleep(1) #do something interesting here
-                #sys.exit()
-		 
-	if number == 3:	
-		time.sleep(1) #do something interesting here
-                #sys.exit()
-
-	if number == 4:
-		time.sleep(1) #do something interesting here
-                #sys.exit()
+     		sys.exit()
+ 
 	
 #set size of the screen
 size = width, height = 240, 320
@@ -80,18 +85,16 @@ screen = pygame.display.set_mode(size)
 
 #set up the fixed items on the menu
 screen.fill(blue) #change the colours if needed
-exit=pygame.image.load("exit.tiff")
-screen.blit(exit,(200,130))
 
 #Add buttons and labels
-make_button("Straight Line", 20, 10, white)
-make_button("Minimal Maze", 20, 45, white)
+make_button("Speed", 20, 10, white)
+make_button("Maze", 125, 10, white)
 make_button("Rainbow", 20, 80, white)
-make_button("Golf", 20, 115, white)
+make_button("Golf", 125, 80, white)
 make_button("Pi Noon", 20, 150, white)
-make_button("Obstacle Course", 20, 185, white)
-make_button("Duck Shoot", 20, 220, white)
-make_button("Remote Control", 20, 255, white)
+make_button("Obstacle", 125, 150, white)
+make_button("Shooting", 20, 220, white)
+make_button("RC", 125, 220, white)
 make_button("Exit", 20, 290, white)
 
 
@@ -104,9 +107,9 @@ while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.MOUSEBUTTONDOWN: 
 			print "screen pressed" #for debugging purposes
-			pos = (event.pos[0], event.pos[1]) #(pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
-			print pos #for checking
-			pygame.draw.circle(screen, white, pos, 2, 0) #for debugging purposes - adds a small dot where the screen is pressed
+			pos = (event.pos[0], event.pos[1])
+			#print pos #for checking
+			#pygame.draw.circle(screen, white, pos, 2, 0) #for debugging purposes - adds a small dot where the screen is pressed
 			on_click(pos)
 
 #ensure there is always a safe way to end the program if the touch screen fails
