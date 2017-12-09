176afc1acd6a36609ace95cbd70e07814ef4a415
Rob Berwick <rob.berwick@gmail.com>
2017-12-09 13:42:01 +0000

    Use common logging conf

--- remote_control.py
+++ remote_control.py
@@ -6,56 +6,50 @@ from approxeng.input.selectbinder import ControllerResource
 import math
 import time
 import logging
-from drivetrain import Drivetrain
+# from drivetrain import Drivetrain
 
-logging.basicConfig(
-    filename='piradigm.log',
-    level=logging.DEBUG,
-    format='%(asctime)s %(message)s'
-)
-
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('piradigm.' + __name__)
 
 
 class RC():
     def __init__(self, timeout=120):
         time.sleep(0.01)
-        logging.info("initialising RC")
+        logger.info("initialising RC")
         self.timeout = timeout
         self.start_time = time.clock()
         self.exponential = 2
         self.name = "RC"
         self.killed = False
-        self.drive = Drivetrain(timeout=self.timeout)
+        # self.drive = Drivetrain(timeout=self.timeout)
 
     def run(self):
-        logging.info("running RC challenge")
+        logger.info("running RC challenge")
         try:
             try:
                 with ControllerResource() as joystick:
-                    logging.info('Found a joystick and connected')
+                    logger.info('Found a joystick and connected')
                     while joystick.connected and not self.should_die:
                         rx, ry = joystick['rx', 'ry']
-                        logging.debug("joystick L/R: %s, %s" % (rx, ry))
+                        logger.debug("joystick L/R: %s, %s" % (rx, ry))
                         rx = self.exp(rx, self.exponential)
                         ry = self.exp(ry, self.exponential)
-                        self.drive.move(rx, ry)
+                        # self.drive.move(rx, ry)
                         time.sleep(0.05)
 
                 # Joystick disconnected...
-                logging.info('Connection to joystick lost')
+                logger.info('Connection to joystick lost')
             except IOError:
                 # No joystick found, wait for a bit before trying again
-                logging.info('Unable to find any joysticks')
+                logger.info('Unable to find any joysticks')
                 time.sleep(1.0)
 
         except KeyboardInterrupt:
             # CTRL+C exit, disable all drives
-            logging.info("killed from keyboard")
+            logger.info("killed from keyboard")
         finally:
-            logging.info("stopping")
-            self.drive.stop()
-            logging.info("bye")
+            logger.info("stopping")
+            # self.drive.stop()
+            logger.info("bye")
 
     @property
     def should_die(self):
@@ -68,7 +62,7 @@ class RC():
         return min(max_val, max(min_val, val))
 
     def stop(self):
-        logging.info("RC challenge stopping")
+        logger.info("RC challenge stopping")
         self.killed = True
 
     def exp(self, demand, exp):
