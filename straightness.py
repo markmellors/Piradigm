b7771a96fba182029a990b24ba47b8fd0c6c5e9b
Mark Mellors <Mark.Mellors@gmail.com>
2017-12-11 21:39:20 +0000

    added some more parameters

--- straightness.py
+++ straightness.py
@@ -3,11 +3,12 @@ from drivetrain import Drivetrain
 import time
 
 MOVE_TIME = float(5)
-TURN_CORRECTION = 0.015
+TURN_CORRECTION = 0.01 #0.01 good for 0.5 fwd
+MAX_SPEED = 0.5
 drive = Drivetrain(timeout=120)
 
 start_time = time.clock()
-drive.move(TURN_CORRECTION, 1)
+drive.move(TURN_CORRECTION, MAX_SPEED)
 try:
     time.sleep(MOVE_TIME)
     drive.move(0, 0)
