b9e22d66a2de9a8b8791a9494410d12ab571589a
Mark Mellors <Mark.Mellors@gmail.com>
2018-01-23 19:04:10 +0000

    first attempt at simplfiying rainbow

---
+++ img_base_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: Latin
+
+# Load library functions we want
+import logging
+import logging.config
+import os
+import time
+import sys
+sys.path.append('/usr/local/lib/python2.7/site-packages')
+
+import threading
+import pygame
+from pygame.locals import*
+import sgc
+from sgc.locals import *
+import picamera
+import picamera.array
+import cv2
+import numpy
+from fractions import Fraction
+from base_challenge import BaseChallenge
--- rainbow.py
+++ rainbow.py
@@ -3,24 +3,7 @@
 # coding: Latin
 
 # Load library functions we want
-import logging
-import logging.config
-import os
-import time
-import sys
-sys.path.append('/usr/local/lib/python2.7/site-packages')
-
-import threading
-import pygame
-from pygame.locals import*
-import sgc
-from sgc.locals import *
-import picamera
-import picamera.array
-import cv2
-import numpy
-from fractions import Fraction
-from base_challenge import BaseChallenge
+import img_base_class import *
 
 logging.config.fileConfig('logging.ini')
 logger = logging.getLogger('piradigm.' + __name__)
