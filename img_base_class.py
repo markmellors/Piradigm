4a680642d78142f53526cba5ea39176537605615
Mark Mellors <Mark.Mellors@gmail.com>
2018-04-12 20:36:28 +0000

    added common image processsing functions to img_base_class

--- img_base_class.py
+++ img_base_class.py
@@ -57,3 +57,100 @@ class ImageCapture(threading.Thread):
             else:
                 yield self.processor.stream
                 self.processor.event.set()
+
+def threshold_image(image, limits):
+        '''function to find what parts of an image lie within limits.
+        returns the parts of the original image within the limits, and the mask'''
+        hsv_lower, hsv_upper = limits
+       
+        mask = wrapping_inRange(
+            image,
+            numpy.array(hsv_lower),
+            numpy.array(hsv_upper)
+        )
+        return mask
+
+def find_largest_contour(image):
+        '''takes a binary image and returns coordinates, size and contourobject of largest contour'''
+        contourimage, contours, hierarchy = cv2.findContours(
+            image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+        )
+        # Go through each contour
+        found_area = 1
+        found_x = -1
+        found_y = -1
+        biggest_contour = None
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if found_area < area:
+                found_area = area
+                M = cv2.moments(contour)
+                found_x = int(M['m10']/M['m00'])
+                found_y = int(M['m01']/M['m00'])
+                biggest_contour = contour
+        return found_x, found_y, found_area, biggest_contour
+
+def colour_of_contour(image, contour):
+    '''Returns the mean of each channel of a given contour in an image'''
+    image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+    if contour is not None:
+        mask = numpy.zeros(image.shape[:2], dtype="uint8")
+        cv2.drawContours(mask, [contour], -1, 255, -1)
+        mask = cv2.dilate(mask, None, iterations=1) #erode makes smaller, dilate makes bigger
+        mean, stddev = cv2.meanStdDev(image, mask=mask)
+        lower = mean - 1.5 * stddev
+        upper = mean + 1.5 * stddev
+    else:
+        lower = None
+        upper = None
+    return rgb2hsv(*lower), rgb2hsv(*upper)
+
+def rgb2hsv(r, g, b):
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = df/mx * 255
+    v = mx * 255
+    h = h * 180 / 360 #to covnert to opencv equivalent hue (0-180)
+    return h, s, v
+
+def wrapping_inRange(image, lower_limit, upper_limit):
+    '''function to behave like opencv imrange, but allow hue to wrap around
+    if hue in lower limit is higher than hue in upper limit, then it will use the wrapped range''' 
+    h_lower, s_lower, v_lower = lower_limit
+    h_upper, s_upper, v_upper = upper_limit
+    if h_lower > h_upper:
+        hsv_lower = (0, s_lower, v_lower)
+        hsv_upper = (h_upper, s_upper, v_upper)
+        imrange1 = cv2.inRange(
+            image,
+            numpy.array(hsv_lower),
+            numpy.array(hsv_upper)
+        )
+        hsv_lower = (h_lower, s_lower, v_lower)
+        hsv_upper = (180, s_upper, v_upper)
+        imrange2 = cv2.inRange(
+            image,
+            numpy.array(hsv_lower),
+            numpy.array(hsv_upper)
+        )
+        imrange =  cv2.bitwise_or(imrange1, imrange2)
+    else:
+        imrange = cv2.inRange(
+            image,
+            numpy.array(lower_limit),
+            numpy.array(upper_limit)
+        )
+    return imrange
